e7aa94504b45d3c0c60127ccb82e1376c9fb9981
Nibbles 2bits <nibbles@speakeasy.net>
2012-03-05 13:16:07 -0800

    rm the extra formulas

--- Library/Formula/youtube-dl.rb
+++
@@ -1,8 +0,0 @@
-require 'formula'
-
-class YoutubeDl < ScriptFileFormula
-  url 'https://github.com/rg3/youtube-dl/raw/2012.01.08b/youtube-dl'
-  homepage 'http://rg3.github.com/youtube-dl/'
-  sha256 '5bfd6ed0e692c9047c81498b70dbe4929a2079c2dfa4018862bb3d34175e988c'
-  version '2012.01.08b'
-end
